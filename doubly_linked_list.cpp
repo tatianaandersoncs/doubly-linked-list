2bb2da74d9ba940390b418f0e9ae1749add58667
tatianaandersoncs <101019509+tatianaandersoncs@users.noreply.github.com>
2025-09-17 22:54:19 -0400

    Create doubly_linked_list.cpp
    Doubly Linked List Class created with methods prototypes

---
+++ doubly_linked_list.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    // Insert at the end
+    void displayAll() {
+
+    }
+
+    // Insert at the end
+    void append(int val) {
+
+    }
+
+    // Insert at the front
+    void prepend(int val) {
+
+    }
+
+    // Insert between first and last, at possition pos
+    void insert(int val, int pos) {
+
+    }
+
+    // Remove from position pos
+    void removeFromPos(int pos) {
+
+    }
+
+    // Find value val and remove it
+    void removeValue(int pos) {
+
+    }
+
+    // Find and return the node with given value
+    Node* search(int value) {
+
+    }
+
+    // Free all memory
+    ~DoublyLinkedList() {
+
+    }
+}
